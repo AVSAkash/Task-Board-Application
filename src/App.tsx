e463c871e690a3d15afb95a4d8523611f63382bf
AVSAkash <148715408+AVSAkash@users.noreply.github.com>
2025-10-01 16:22:20 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,11 @@ import BoardDetail from "./pages/BoardDetail";
 
 function App() {
   return (
+    // Set up the application's routes.
     <Routes>
+      {/* The root path displays the list of all boards. */}
       <Route path="/" element={<BoardView />} />
+      {/* A dynamic path to display the details of a specific board. */}
       <Route path="/board/:id" element={<BoardDetail/>} />
     </Routes>
   );
